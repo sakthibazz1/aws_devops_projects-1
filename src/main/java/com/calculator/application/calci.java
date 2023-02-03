8633527fdabbfe97d75d53c41155f5d52587d6d0
chethan <chethangowda@gamil.com>
2023-02-03 13:20:02 +0530

    all maven project

---
+++ src/main/java/com/calculator/application/calci.java
@@ -0,0 +1,26 @@
+package main.java.com.calculator.application;
+
+public class calci {
+	public static int div(int a,int b)
+	{
+		return a/b;
+	}
+	public static float mult(int a,int b) {
+
+		return a*b;
+	}
+	public static int add(int a,int b) {
+
+		return a+b;
+	}
+	public static int sub(int a,int b) {
+
+		return a-b;
+	}
+	public static int modl(int a,int b) {
+		return a%b;
+
+	}
+
+}
+
